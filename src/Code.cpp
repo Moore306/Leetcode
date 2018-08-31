f85145a30ba4ae56d8f8c89f07fe6260e66eebc0
Moore306 <yangyanglan1@163.com>
2018-09-01 00:56:51 +0800

    test map :: insert map

--- src/Code.cpp
+++ src/Code.cpp
@@ -6,41 +6,25 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include <map>
+#include <string>
 #include <iostream>
-#include <cstdio>
-#include <stack>
-#include <queue>
-
-#include <deque>
 using namespace std;
+
 int main()
 {
-	stack<int> S;
-
-	S.push(1);
-	S.push(2);
-	S.push(3);
-
-	cout<<S.top()<<endl;
-	S.pop();
-	cout<<S.top()<<endl;
-	S.pop();
-	cout<<S.top()<<endl;
-	S.pop();
-	//cout<<S.top()<<endl;
-	cout<<"**************************"<<endl;
-	queue<int> que;
-	que.push(1);
-	que.push(2);
-	que.push(3);
-	cout<<"size "<<que.size()<<endl;
-	cout<<que.front()<<" "<<que.back()<<endl;
-	cout<<que.front()<<endl;
-	que.pop();
-	cout<<que.front()<<endl;
-	que.pop();
-	cout<<que.front()<<endl;
-	que.pop();
-
-	return 0;
+    map<int, string> mapStudent;
+    mapStudent.insert(pair<int, string>(1, "student_one"));
+    //mapStudent.insert(pair<int, string>(1, "student_one22"));
+    //mapStudent[1]="sssff";
+    //mapStudent[1]="sssfsafaf";
+    mapStudent.insert(pair<int, string>(2, "student_two"));
+    mapStudent.insert(pair<int, string>(3, "student_three"));
+    map<int, string>::iterator  iter;
+    for(iter = mapStudent.begin(); iter != mapStudent.end(); iter++)
+    {
+        cout<<iter->first<<" "<<iter->second<<endl;
+    }
+    system("pause");
+    return 0;
 }
