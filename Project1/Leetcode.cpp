e2c7fb78369dda151c561ba5769da03fb0cc0b56
e <2312118379@qq.com>
2018-09-12 23:28:25 +0800

    KMP算法测试

--- Project1/Leetcode.cpp
+++ Project1/Leetcode.cpp
@@ -1,65 +1,59 @@
 #include<iostream>
+#include<vector>
 #include<string>
-#include<set>
-/*
-asdfjkl
-5
-as
-asd
-df
-fjkl
-jkl
-*/
 using namespace std;
-void partition(string& w, set<string>& dic, int &st);
+int KMP(string t,string p);
+int KMP(string t, string p)
+{
+	int tl = t.length();
+	int pl = p.length();
+	int* next = new int[pl];
+	next[0] = -1;
+	int k = -1;
+	int j = 0;
+	while(j < pl)
+	{
+		if (k == -1 || p[k] == p[j])
+		{
+			k++;
+			j++;
+			if (p[j] != t[k])
+				next[j] = k ;
+			else
+				next[j] = next[k];
+		}
+		else {
+			k = next[k];
+		}
+	}
+//  ¿ªÊ¼Æ¥Åä
+	int i = 0; j = 0;
+	while (i < tl&&j < pl)
+	{
+		if (j == -1 || t[i] == p[j])
+		{
+			i++;
+			j++;
+		}
+		else
+			j = next[j];
+
+	}
+	if (j == pl)
+		return i - j;
+	else
+		return -1;
+}
 
 int main()
 {
-	set<string> dict;
-	int N;
-	string s, word;
-	cin >> word;
-	cin >> N;
-	for (int i = 0; i<N; i++)
-	{
-		cin >> s;
-
-		dict.insert(s);
-
-	}
-	int st = 0;
-	int ed = 0;
-	 while(st!=word.length())
-	{
-		partition(word, dict, st);
-	}
-	//cout << "st  " << st << endl;
-
+	string test = "ahfihhfiabacdejfijf";
+	string p = "abacde";
+	int k = KMP(test, p); 
+	
+	cout <<"test: " +test << endl;
+	cout << "p  " + p << endl;
+	cout << "k " << k << endl;
 	system("pause");
 	return 0;
-}
-void partition(string& w, set<string>& dic, int &st)
-{
-	string  temp = w.substr(st, w.length() - st);
-	string long_s;
-	if (temp.length() == 1)
-	{
-		cout << w << endl;
-		return;
-
-	}
-	for (int i = 1; i<temp.length(); i++)
-	{
-		string l = temp.substr(0, i+1);
-		if (dic.find(l) != dic.end())
-		{
-			long_s = l;
-			//cout << "999 " << long_s << endl;
-		}
-
-	}
-	cout << long_s << " -- " ;
-	st = st + long_s.length();
-	//cout << st << "  st" << endl;
-	return;
 }
